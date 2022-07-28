da2f2461bf2bf25c54fd1d93272a12b60e222a5c
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-28 15:07:36 +0800

    Add reference category to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_062455) do
+ActiveRecord::Schema.define(version: 2022_07_28_070505) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2022_07_28_062455) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
