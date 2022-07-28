bc0bb6ce476891216d16990c84fa4a4d0d85e5fc
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-28 18:35:47 +0800

    Add image functions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,14 @@
 class Post < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
+  validates_presence_of :address
 
   belongs_to :user
   belongs_to :category
   belongs_to :region
   belongs_to :city
   has_many :comments
+  mount_uploader :image, ImageUploader
 
   before_create :generate_unique_number
 
