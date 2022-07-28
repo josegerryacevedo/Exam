216e9a7c657c3938c84b047988fd091c0939839f
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-28 17:47:26 +0800

    Create table for city/api

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_091145) do
+ActiveRecord::Schema.define(version: 2022_07_28_094512) do
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
