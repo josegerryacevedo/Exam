64c45d0fda82cdd1d3cef1a12d1903697bed7173
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-28 14:42:00 +0800

    Create table for category "type of disaster"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_060304) do
+ActiveRecord::Schema.define(version: 2022_07_28_062455) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
